49cc6957640d98a0be566959816d58cc3bf2b9fb
Jeff Hastings <jeffmhastings@gmail.com>
2018-04-19 13:53:44 -0400

    Add cluster role

--- kubernetes/structures_rbac.go
+++ kubernetes/structures_rbac.go
@@ -4,29 +4,47 @@ import (
 	rbacv1 "k8s.io/client-go/pkg/apis/rbac/v1beta1"
 )
 
-func flattenRoleRef(in rbacv1.RoleRef) interface{} {
-	att := make(map[string]interface{})
-	att["api_group"] = in.APIGroup
-	att["kind"] = in.Kind
-	att["name"] = in.Name
-
-	return att
-}
-
-func flattenSubjects(in []rbacv1.Subject) []interface{} {
-	att := make([]interface{}, len(in))
-	for i, v := range in {
-		obj := make(map[string]interface{})
-		obj["kind"] = v.Kind
-		if v.APIGroup != "" {
-			obj["api_group"] = v.APIGroup
-		}
-		obj["name"] = v.Name
-		obj["namespace"] = v.Namespace
-		att[i] = obj
+func expandRules(in []interface{}) []rbacv1.PolicyRule {
+	if len(in) == 0 {
+		return []rbacv1.PolicyRule{}
 	}
 
-	return att
+	rules := make([]rbacv1.PolicyRule, len(in))
+
+	for i, v := range in {
+		rule := v.(map[string]interface{})
+
+		if verbs, ok := rule["verbs"].([]interface{}); ok {
+			rules[i].Verbs = expandStringSlice(verbs)
+		}
+		if api_groups, ok := rule["api_groups"].([]interface{}); ok {
+			rules[i].APIGroups = expandStringSlice(api_groups)
+		}
+		if resources, ok := rule["resources"].([]interface{}); ok {
+			rules[i].Resources = expandStringSlice(resources)
+		}
+		if resource_names, ok := rule["resource_names"].([]interface{}); ok {
+			rules[i].ResourceNames = expandStringSlice(resource_names)
+		}
+		if non_resource_urls, ok := rule["non_resource_urls"].([]interface{}); ok {
+			rules[i].NonResourceURLs = expandStringSlice(non_resource_urls)
+		}
+	}
+	return rules
+}
+
+func flattenRules(in []rbacv1.PolicyRule) []interface{} {
+	rules := make([]interface{}, len(in))
+	for i, v := range in {
+		rule := make(map[string]interface{})
+		rule["verbs"] = v.Verbs
+		rule["api_groups"] = v.APIGroups
+		rule["resources"] = v.Resources
+		rule["resource_names"] = v.ResourceNames
+		rule["non_resource_urls"] = v.NonResourceURLs
+		rules[i] = rule
+	}
+	return rules
 }
 
 func expandRoleRef(in map[string]interface{}) rbacv1.RoleRef {
@@ -35,6 +53,15 @@ func expandRoleRef(in map[string]interface{}) rbacv1.RoleRef {
 		Kind:     in["kind"].(string),
 		Name:     in["name"].(string),
 	}
+}
+
+func flattenRoleRef(in rbacv1.RoleRef) interface{} {
+	att := make(map[string]interface{})
+	att["api_group"] = in.APIGroup
+	att["kind"] = in.Kind
+	att["name"] = in.Name
+
+	return att
 }
 
 func expandSubjects(s []interface{}) []rbacv1.Subject {
@@ -65,4 +92,19 @@ func expandSubjects(s []interface{}) []rbacv1.Subject {
 	}
 
 	return subjects
+}
+func flattenSubjects(in []rbacv1.Subject) []interface{} {
+	att := make([]interface{}, len(in))
+	for i, v := range in {
+		obj := make(map[string]interface{})
+		obj["kind"] = v.Kind
+		if v.APIGroup != "" {
+			obj["api_group"] = v.APIGroup
+		}
+		obj["name"] = v.Name
+		obj["namespace"] = v.Namespace
+		att[i] = obj
+	}
+
+	return att
 }
